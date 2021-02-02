2bec930c2222b18e8ccbb8e93e3ee5965d4c3c80
rmzNadir <dgzrz99@gmail.com>
2021-02-01 18:26:52 -0600

    :construction: routes

--- bloglist-app/blog-list-frontend/src/App.js
+++ bloglist-app/blog-list-frontend/src/App.js
@@ -1,4 +1,8 @@
 import React, { useEffect } from 'react';
+import { Switch, Route, Link, Redirect, useHistory } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { setNotification } from './reducers/notificationReducer';
+import { logoutUser } from './reducers/userReducer';
 
 // Components
 
@@ -6,7 +10,8 @@ import Blogs from './components/Blogs';
 import NewBlogForm from './components/NewBlogForm';
 import LoginForm from './components/LoginForm';
 import Notification from './components/Notification';
-import { useDispatch, useSelector } from 'react-redux';
+import Blog from './components/Blog';
+import Users from './components/Users';
 
 // Styles
 
@@ -15,20 +20,19 @@ import './App.css';
 // Services
 
 import { initializeBlogs } from './reducers/blogsReducer';
-import { setNotification } from './reducers/notificationReducer';
-import { initializeUser, logoutUser } from './reducers/userReducer';
+import { initializeUser } from './reducers/userReducer';
 
 const App = () => {
   const dispatch = useDispatch();
+  const history = useHistory();
   const user = useSelector(({ user }) => user);
 
   useEffect(() => {
-    dispatch(initializeBlogs());
     dispatch(initializeUser());
+    dispatch(initializeBlogs());
   }, [dispatch]);
 
   // Service implementation for handling user logouts
-
   const handleLogout = () => {
     dispatch(logoutUser());
     dispatch(
@@ -40,28 +44,46 @@ const App = () => {
         5
       )
     );
+    history.push('/login');
   };
 
   return (
     <>
-      <Notification />
-      {!user && <LoginForm />}
       {user && (
         <>
+          <div className='nav-bar'>
+            <Link to='/'>home</Link>
+            <Link to='/blogs/new'>new blog</Link>
+            <Link to='/users'>users</Link>
+          </div>
+
           <h2>blogs</h2>
           <div>
-            {user.name} logged in. &nbsp;
+            {user && user.name} logged in. &nbsp;
             <button onClick={handleLogout}>Logout</button>
             <br />
           </div>
-
-          <NewBlogForm />
-
-          <br />
-
-          <Blogs />
         </>
       )}
+      <Notification />
+      <Switch>
+        <Route path='/blogs/new'>
+          <NewBlogForm />
+        </Route>
+        <Route path='/blogs/:id'>
+          <Blog />
+        </Route>
+        <Route path='/blogs'>
+          <Redirect to='/' />
+        </Route>
+        <Route path='/users'>
+          <Users />
+        </Route>
+        <Route path='/login'>
+          {user ? <Redirect to='/' /> : <LoginForm />}
+        </Route>
+        <Route path='/'>{user ? <Blogs /> : <Redirect to='/login' />}</Route>
+      </Switch>
     </>
   );
 };
