78ffc48a22f7829c65d98e46be0051d43abe3b27
rmzNadir <dgzrz99@gmail.com>
2021-02-01 18:42:19 -0600

    :sparkles: users view

--- bloglist-app/blog-list-frontend/src/components/Users.js
+++ bloglist-app/blog-list-frontend/src/components/Users.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { initializeUsers } from '../reducers/usersReducer';
 
 const Users = () => {
+  const dispatch = useDispatch();
+  const users = useSelector(({ users }) => users);
+  console.log('users', users);
+
+  useEffect(() => {
+    dispatch(initializeUsers());
+  }, [dispatch]);
+
   return (
-    <>
-      <h1>Users</h1>
-    </>
+    users && (
+      <>
+        <h1>Users</h1>
+        <div className='users-info-header'>
+          <strong>Name</strong>
+          <strong>Blogs Created</strong>
+        </div>
+        {users.map(({ name, blogsPosted, id }) => (
+          <div key={id} className='users-info-header'>
+            <div>{name}</div>
+            <div>{blogsPosted.length}</div>
+          </div>
+        ))}
+      </>
+    )
   );
 };
 
--- bloglist-app/blog-list-frontend/src/store.js
+++ bloglist-app/blog-list-frontend/src/store.js
@@ -4,11 +4,13 @@ import thunk from 'redux-thunk';
 import blogsReducer from './reducers/blogsReducer';
 import notificationReducer from './reducers/notificationReducer';
 import userReducer from './reducers/userReducer';
+import usersReducer from './reducers/usersReducer';
 
 const reducer = combineReducers({
   blogs: blogsReducer,
   notification: notificationReducer,
   user: userReducer,
+  users: usersReducer,
 });
 
 const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
